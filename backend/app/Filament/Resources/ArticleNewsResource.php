aaeec080dc16243fe105ee2eb3d99a66c37caa7e
RasyidAbdulRauf <rasyidabdulrauf05@gmail.com>
2025-04-29 14:04:33 +0700

    setup backend

---
+++ backend/app/Filament/Resources/ArticleNewsResource.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use App\Models\ArticleNews;
+use Filament\Resources\Resource;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\ArticleNewsResource\Pages;
+use App\Filament\Resources\ArticleNewsResource\RelationManagers;
+
+class ArticleNewsResource extends Resource
+{
+    protected static ?string $model = ArticleNews::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-newspaper';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\FileUpload::make('thumbnail')
+                    ->image()
+                    ->directory('news')
+                    ->required(),
+                Forms\Components\RichEditor::make('content')
+                    ->columnSpan(2)
+                    ->required(),
+                Forms\Components\Select::make('category_id')
+                    ->relationship('category', 'name')
+                    ->required(),
+                Forms\Components\Select::make('author_id')
+                    ->relationship('author', 'name')
+                    ->required(),
+                Forms\Components\Select::make('is_featured')
+                    ->options([
+                        'featured' => 'Featured',
+                        'not_featured' => 'Not Featured',
+                    ])
+                    ->required()
+                    ->columnSpan(2),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\ImageColumn::make('thumbnail'),
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('category.name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('author.name'),
+                Tables\Columns\TextColumn::make('is_featured')
+                    ->badge()
+                    ->color(fn (string $state): string => match ($state) {
+                        'featured' => 'success',
+                        'not_featured' => 'danger',
+                    }),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListArticleNews::route('/'),
+            'create' => Pages\CreateArticleNews::route('/create'),
+            'edit' => Pages\EditArticleNews::route('/{record}/edit'),
+        ];
+    }
+}
